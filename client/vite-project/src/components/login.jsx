16205f0cfe3f5089675f08cfc95672563d14f130
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2026-06-09 13:49:52 +0530

    work on frontend

---
+++ client/vite-project/src/components/login.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import "../styles/login.css";
+import { Link, useNavigate } from "react-router-dom";
+
+function Login() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchData();
+    };
+
+
+    async function fetchData() {
+        try {
+            const response = await fetch(
+                "http://localhost:5000/api/user/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log(data, "data from login");
+                localStorage.setItem("token", data?.data?.token); // Store token for future use
+                navigate("/products");
+            } else {
+                alert(data.message || "Login Failed");
+            }
+        } catch (error) {
+            console.error("Error logging in:", error);
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="login-box">
+                <h2>Login</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter Email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter Password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <button type="submit">Login</button>
+
+                    <p className="auth-link">
+                        Don't have an account? <Link to="/signup">Sign Up</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
+
